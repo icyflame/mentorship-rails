759537a80a09231d93166955b52d4dba27bccfb9
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-31 00:54:08 +0530

    Profile pages check for login.
    Logged in user can see only his/her profile page. All other profile
    pages are out of reach for the user.

--- app/controllers/mentor_controller.rb
+++ app/controllers/mentor_controller.rb
@@ -1,7 +1,14 @@
 class MentorController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  def show
-  end
+	def show
+		if mentor_signed_in?
+			if current_mentor.id == params[:id]
+				redirect_to root_path
+			end
+		else
+			redirect_to root_path
+		end
+	end
 end
--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,7 +1,14 @@
 class StudentController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  def show
-  end
+	def show
+		if student_signed_in?
+			if current_student.id == params[:id]
+				redirect_to root_path
+			end
+		else
+			redirect_to root_path
+		end
+	end
 end
