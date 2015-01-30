fe5b2ded80c67fd9dd9d8911bb2f09a9c4ee4abd
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-31 00:35:22 +0530

    Add the mainpage controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'mainpage/index'
+
   get 'mentor/index'
 
   get 'mentor/show'
@@ -15,7 +17,9 @@ Rails.application.routes.draw do
   resources :student
   resources :mentor
 
-  root 'student#index'
+  post '/loginperson' => 'mainpage#loginperson'
+
+  root 'mainpage#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
