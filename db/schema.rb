6f3b5ead75e4d321d9c0e079d32e322c5d293006
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-30 23:12:47 +0530

    Add the student and the mentor devise models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130163301) do
+ActiveRecord::Schema.define(version: 20150130174121) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,51 @@ ActiveRecord::Schema.define(version: 20150130163301) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "mentors", force: true do |t|
+    t.string   "name"
+    t.integer  "passoutyear"
+    t.string   "department"
+    t.string   "hall"
+    t.string   "field"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "mentors", ["email"], name: "index_mentors_on_email", unique: true
+  add_index "mentors", ["reset_password_token"], name: "index_mentors_on_reset_password_token", unique: true
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "rollnum"
+    t.string   "department"
+    t.integer  "joinyear"
+    t.string   "hall"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+  end
+
+  add_index "students", ["email"], name: "index_students_on_email", unique: true
+  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
 end
