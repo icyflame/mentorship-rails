425bba135a23545825597c2edf6a07b575610bce
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-30 23:49:58 +0530

    Add basic validation of the fields.
    - None of the fields can be left empty in either student or the mentor
    models.

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -3,4 +3,5 @@ class Mentor < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_presence_of :name, :department, :passoutyear, :hall, :field
 end
